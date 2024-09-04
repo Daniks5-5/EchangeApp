6f9d9da8f99444655da39bde14115919df90b2e5
Daniks5-5 <danilworkup@gmail.com>
2024-09-04 17:50:46 +0300

    add logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import EchangeSection from './components/EchangeSection/EchangeSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <>
-    </>
+  <EchangeSection />
   )
 }
 
---
+++ src/components/EchangeSection/EchangeSection.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import "./EchangeSection.css";
+
+function EchangeSection() {
+    const [rates, setRates] = useState(null); // Для хранения курсов валют
+    const [loading, setLoading] = useState(true);  // Для отслеживания состояния загрузки
+    const [error, setError] = useState(null); // Для хранения ошибок, если они возникнут
+    const [searchTerm, setSearchTerm] = useState('');  // Для хранения значения из input
+
+    useEffect(() => {
+        // Запрос данных с API ЦБ РФ
+        fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                setRates(data.Valute); // Сохраняем данные о курсах валют
+                setLoading(false);     // Устанавливаем состояние загрузки в false
+            })
+            .catch(error => {
+                setError(error);       // Устанавливаем ошибку, если запрос завершился с ошибкой
+                setLoading(false);
+            });
+    }, []); // Пустой массив зависимостей означает, что useEffect выполнится только один раз при монтировании компонента
+
+    if (loading) return <p>Загрузка...</p>;
+    if (error) return <p>Ошибка: {error.message}</p>;
+
+   // Фильтруем вывод по значению input
+   const filteredRates = rates
+   ? Object.values(rates).filter(rate =>
+       rate.Name.toLowerCase().includes(searchTerm.toLowerCase()))
+   : [];
+
+   return (
+    <div>
+        <input
+            id='input'
+            placeholder='Поиск'
+            type='text'
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <h1>Курсы валют</h1>
+
+        {searchTerm.trim() === '' ? (
+            <div>Введите название валюты</div>
+        ) : filteredRates.length === 0 ? (
+            <div>Валюта не найдена</div>
+        ) : (
+            <ul>
+                {filteredRates.map(rate => (
+                    <p key={rate.ID}>
+                        {rate.CharCode}: {rate.Name} - {rate.Value} RUB
+                    </p>
+                ))}
+            </ul>
+        )}
+    </div>
+);
+}
+
+export default EchangeSection;
